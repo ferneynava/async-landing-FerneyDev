c0d6d66ed604c7993d99bae7a86064b762c77a38
ferneynava <ferneynava@gmail.com>
2023-04-30 15:07:22 -0500

    :sparkles: [new] features anchor video
    [new] features anchor video

--- src/assets/main.js
+++ src/assets/main.js
@@ -22,19 +22,21 @@ async function fetchData (urlApi) {
     const videos = await fetchData(API)
     const view = `
     ${videos.items.map(video => `
+      <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">  
         <div class="group relative">
-        <div
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
-        </div>
-        <div class="mt-4 flex justify-between">
+          <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div> 
+        
+          <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
             ${video.snippet.title}
             </h3>
+          </div>
         </div>
-        </div>
-    `).slice(0, 4).join('')}
+      
+    `).slice(0, 8).join('')}
     `
     content.innerHTML = view
   } catch (error) {
